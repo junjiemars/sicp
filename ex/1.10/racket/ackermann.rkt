1db3373c690ebdc49fb785a0e8d22a58e2c4647d
南山竹 <junjiemars@gmail.com>
2016-10-09 17:33:16 +0800

    crazy ackermann's fn

--- ex/1.10/racket/ackermann.rkt
+++ ex/1.10/racket/ackermann.rkt
@@ -8,6 +8,11 @@
     [else (A (- x 1) (A x (- y 1)))]))
 
 
+(define (f n) (A 0 n))   ; => 2n
+(define (g n) (A 1 n))   ; => 2^{n}
+(define (h n) (A 2 n))   ; => 2^2^...(n-1 times)
+(define (k n) (* 5 n n)) ; => 5n^{2}
+
 #|
 
 (A 1 10) => 1024
