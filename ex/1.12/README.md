6e769ef3f6a545af3f4cb1d288dc30a0097d0582
南山竹 <junjiemars@gmail.com>
2016-10-17 22:42:41 +0800

    Refine: n! = n..r! * (r-1)!

---
+++ ex/1.12/README.md
@@ -0,0 +1,16 @@
+# Pascal Triangle
+The ___Row___ from the topmost to bottom, the ___Column___ from the leftmost to right, 
+based on 0.
+For any non-negative integer n (rows) and any integer k (columns) between 0 and n, inclusive.
+
+## Entry
+
+### Pingala's formula
+
+
+### Mahāvīra's formula
+
+
+## References
+* [Pascal's triangle](https://en.wikipedia.org/wiki/Pascal%27s_triangle)
+
--- ex/1.12/pascal.rkt
+++ ex/1.12/pascal.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+; Row, Column start from 1
+
 (define (pascal-triangle-add row col)
   (cond
    [(> col row) 0]
@@ -8,13 +10,16 @@
    [else (+ (pascal-triangle-add (sub1 row) (sub1 col))
             (pascal-triangle-add (sub1 row) col))]))
 
+
 (define (pascal-triangle-mul row col)
-  (let ([factorial (λ (n)
-                     (define (iter x n)
-                       (if (zero? n)
-                           x
-                           (iter (* x n) (sub1 n))))
-                     (iter 1 n))])
+  (let* ([factorial (λ (n)
+                      (define (iter x n)
+                        (if (zero? n)
+                            x
+                            (iter (* x n) (sub1 n))))
+                      (iter 1 n))]
+         [row (sub1 row)]
+         [col (sub1 col)])
     (cond
      [(> col row) 0]
      [(< col 0) 0]
@@ -22,5 +27,20 @@
      [else (/ (factorial row)
               (* (factorial col) (factorial (- row col))))])))
 
-
+(define (pascal-triangle-mul! row col)
+  (let* ([factorial (λ (r n)
+                      (define (iter x r n)
+                        (if (= r n)
+                            x
+                            (iter (* x n) r (sub1 n))))
+                      (iter 1 r n))]
+         [row (sub1 row)]
+         [col (sub1 col)])
+    (cond
+     [(> col row) 0]
+     [(< col 0) 0]
+     [(= col 1) 1]
+     [else (let ([col! (factorial 1 (sub1 col))])
+             (/ (* col! (factorial col row))
+                (* col! (factorial 1 (- row col)))))])))
 
