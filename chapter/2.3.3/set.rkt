4df769cea2f121345e62454ae3951bb2e9fc158d
南山竹 <junjiemars@gmail.com>
2017-03-24 23:03:43 +0800

    unordered set in Racket

--- chapter/2.3.3/set.rkt
+++ chapter/2.3.3/set.rkt
@@ -6,6 +6,18 @@
         (else (element-of-set? x (cdr set)))))
 
 (define (adjoin-set x set)
-  (if (element-of-set? x set)
-      set
-      (cons x set)))
+  (cond ((element-of-set? x set) #f)
+        (else (cons x set))))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) null)
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        (else (union-set (cdr set1)
+                         (adjoin-set (car set1) set2)))))
+
