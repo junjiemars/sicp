9ea1d1b24480653731422dd5da86d1837b18f236
南山 <junjiemars@gmail.com>
2017-03-25 21:39:16 +0800

    set as ordered list in CL

--- chapter/2.3.3/set-as-unordered-list.lisp
+++ chapter/2.3.3/set-as-unordered-list.lisp
@@ -6,14 +6,15 @@
         ((element-of-set? x (cdr set)))))
 
 (defun adjoin-set (x set)
-  "Takes an object `x' and a `set' as arguments of the original set and also the 
-adjoined element."
+  "Takes an object `x' and a `set' as arguments of the original set 
+   and also the adjoined element."
   (cond ((element-of-set? x set) set)
         ((cons x set))))
 
 (defun intersection-set (set1 set2)
-  "Computes the intersection of `set1' and `set2', which is the set containing only
-only elements that appear in both arguments."
+  "Computes the intersection of `set1' and `set2', 
+   which is the set containing only only elements 
+   that appear in both arguments."
   (cond ((or (null set1) (null set2)) nil)
         ((element-of-set? (car set1) set2)
          (cons (car set1)
@@ -21,8 +22,8 @@ only elements that appear in both arguments."
         ((intersection-set (cdr set1) set2))))
 
 (defun union-set (set1 set2)
-  "Computes the union of two sets, which is the set containing each element that 
-appears in either argument."
+  "Computes the union of two sets, which is the set containing 
+   each element that appears in either argument."
   (cond ((null set1) set2)
         ((union-set (cdr set1)
                     (adjoin-set (car set1) set2)))))
